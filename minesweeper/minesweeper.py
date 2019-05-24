17b2c67476b6f543a7f6775cf3324c7da8a15a43
neyirerdeser <neyirerdeser@gmail.com>
2019-05-24 18:56:05 +0300

    removed test mode

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,7 +1,7 @@
 import random
 
 
-class Game:
+class Minesweeper:
     revealed = []
     flagged = []
     board = []
@@ -135,67 +135,57 @@ class Game:
                 if (i, j) not in self.flagged:
                     self.reveal(i, j)
 
-    def play(self, test, mode, i, j):
-        move = self.get_move(test, mode, i, j)
-        key = move[0]
-        i = move[1]
-        j = move[2]
+    def play(self, move=None):
+        i, j, key = self.make_move(move)
         if key == 'open':
             self.reveal(i, j)
             if self.board[i][j] == 9:
                 return False
         if key == 'flag':
             self.flag(i, j)
-        if not test:
-            self.print_game()
         return True
 
-    def get_move(self, test, mode, i, j):
+    def make_move(self, move):
+        if move is None:
+            print('to open follow format <row> <column>\n'
+                  'to flag/unflag follow <row> <column> f')
+            click = input().split()
+            while not self.valid_click(click):
+                click = input().split()
+        else:
+            click = move.split()
+        return self.valid_click(click)
+
+    def valid_click(self, click):
         end = self.size - 1
-        if not test:
-            print('coordinates in format <row>  <column>\nto place a flag follow the format: f <row> <column> ')
-            while True:
-                coord = input()
-                click = coord.split(' ')
-                if len(click) == 2:
-                    try:
-                        r = int(click[0])
-                        c = int(click[1])
-                    except (TypeError, ValueError):
-                        print('both coordinates must be integers')
-                        continue
-                    if r > end or c > end:
-                        print('location outside the game, careful first box is 0 0. '
-                              'choose a different box (row  column): ')
-                    elif (r, c) in self.revealed:
-                        print('box already open, choose a different box (row  column): ')
-                    elif (r, c) in self.flagged:
-                        print('box is flagged, to unflag enter the same command as flagging')
-                    else:
-                        return tuple(('open', r, c))
-                elif len(click) == 3:
-                    if click[0] != 'f':
-                        print(
-                            'please give two numbers seperated by space to enter coordinates\n'
-                            'to place a flag follow the format: f <row> <column>')
-                        continue
-                    try:
-                        r = int(click[1])
-                        c = int(click[2])
-                    except (TypeError, ValueError):
-                        print('both coordinates must be integers')
-                        continue
-                    if r > end or c > end:
-                        print('location outside the game, careful first box is 0 0. '
-                              'choose a different box (row  column): ')
-                    elif (r, c) in self.revealed:
-                        print('box already open, choose a different box (row  column): ')
-                    else:
-                        return tuple(('flag', r, c))
-                else:
-                    print('please give two numbers separated by space to enter coordinates')
-        else:  # in test
-            return tuple((mode, i, j))
+        digits = len(click)
+        if digits not in (2, 3):
+            print('please give two numbers separated by space to enter coordinates')
+            return False
+        try:
+            r = int(click[0])
+            c = int(click[1])
+        except (TypeError, ValueError):
+            print('both coordinates must be integers')
+            return False
+        if r > end or r < 0 or c > end or r < 0:
+            print('location outside the game, careful first box is 0 0. ')
+            return False
+        if (r, c) in self.revealed:
+            print('box already open')
+            return False
+        if digits == 2:
+            mode = 'open'
+            if (r, c) in self.flagged:
+                print('box is flagged, to unflag type %d %d f' % (r, c))
+                return False
+        elif click[2] == 'f':
+            mode = 'flag'
+        else:
+            print('to (un)flag type f after the coordinates')
+            return False
+
+        return r, c, mode
 
     def print_game(self):
         def horizontal(length):
@@ -204,6 +194,7 @@ class Game:
             for l in range(length):
                 print('----', end='')
             print()
+
         print('  | ', end='')
         for i in range(self.size):
             print(i % 10, end='   ')
@@ -231,7 +222,7 @@ class Game:
               'some notes for after you choose the level you want to play\n'
               '* to open a box, you will enter your coordinates in the format:'
               ' <row> <column> of your desired number (eg. 2 5)\n'
-              '* to (un)flag a box put an 'f' in front of your coordinates (eg. f 4 3)\n'
+              '* to (un)flag a box put an 'f' at the end of your coordinates (eg. 4 3 f)\n'
               '* game ends when all unopened or flagged boxes are mines\n'
               '* empty boxes are shown with a . in them for convenience\n'
               '* keep in mind that the upper left corner has coordinates 0 0\n'
@@ -240,18 +231,14 @@ class Game:
               'lets start :)\n')
 
     @classmethod
-    def get_level(cls, test, lvl=None):
-        if not test:
-            print('level of difficulty:\n'
-                  '1. Beginner\n2. Intermediate\n3. Expert\nYour choice: ')
+    def get_level(cls):
+        print('level of difficulty:\n'
+              '1. Beginner\n2. Intermediate\n3. Expert\nYour choice: ')
         level = 0
         valid = False
         while not valid:
             try:
-                if lvl is None:
-                    level = int(input())
-                else:
-                    level = lvl
+                level = int(input())
             except (TypeError, ValueError):
                 print('level must be a number')
                 continue
@@ -267,71 +254,52 @@ class Game:
         self.print_game()
         print('GAME OVER\nmines exploded: %d\nplay again? (Y/N)' % mines_left)
 
-    def won(self, test):
+    def won(self):
         for (a, b) in self.flagged:
             if self.board[a][b] != 9:
                 return False
-        if not test:
-            print('CONGRATULATIONS\nplay again? (Y/N)')
+        print('CONGRATULATIONS\nplay again? (Y/N)')
         return True
 
     @classmethod
-    def minesweeper(cls, test, mode=None, i=None, j=None, lvl=None):
+    def run_with_moves_list(cls, moves):
+        turn = 0
+        level = 1
+        game = Minesweeper(level)
+        game.print_game()
+        while game.play(moves[turn]):
+            game.print_game()
+            mines_left = game.mines - len(game.flagged)
+            hidden = game.size ** 2 - len(game.revealed) - len(game.flagged)
+            if (mines_left == 0 or hidden == mines_left) and game.won():
+                break
+            turn += 1
+            if turn >= len(moves):
+                print('end of scenario')
+                break
+        else:
+            game.print_game()
+            game.lost()
+
+    @classmethod
+    def run_with_user_input(cls):
         playing = True
         while playing:
-            level = cls.get_level(test, lvl)
-            game = Game(level)
+            level = cls.get_level()
+            game = Minesweeper(level)
             game.print_game()
-            turn = 0
-            curr_mode = None
-            curr_i = None
-            curr_j = None
-            if test:
-                if mode[turn] is None or mode[turn] not in ('open', 'flag'):
-                    raise Exception('mode must be open or flag')
-                elif i[turn] is None or j[turn] is None:
-                    raise Exception('test mode needs coordinates')
-                else:
-                    curr_mode = mode[turn]
-                    curr_i = i[turn]
-                    curr_j = j[turn]
-
-            while game.play(test, curr_mode, curr_i, curr_j):
-
+            while game.play():
+                game.print_game()
                 mines_left = game.mines - len(game.flagged)
                 hidden = game.size ** 2 - len(game.revealed) - len(game.flagged)
-                if (mines_left == 0 or hidden == mines_left) and game.won(test):
+                if (mines_left == 0 or hidden == mines_left) and game.won():
                     break
-                turn += 1
-                if test:
-                    if turn >= len(mode):
-                        curr_mode = 'open'
-                        while True:
-                            curr_i = random.randint(0, game.size - 1)
-                            curr_j = random.randint(0, game.size - 1)
-                            if (curr_i, curr_j) not in game.revealed:
-                                break
-                            elif hidden == 0:
-                                # then coordinates must be in game.flagged
-                                # then unflag and play
-                                game.flag(curr_i, curr_j)
-                                break
-                    else:
-                        if mode[turn] is None or mode[turn] not in ('open', 'flag'):
-                            raise Exception('mode must be open or flag')
-                        elif i[turn] is None or j[turn] is None:
-                            raise Exception('test mode needs coordinates')
-                        else:
-                            curr_mode = mode[turn]
-                            curr_i = i[turn]
-                            curr_j = j[turn]
             else:
+                game.print_game()
                 game.lost()
             valid = False
             while not valid:
-                answer = 'N'
-                if not test:
-                    answer = input()
+                answer = input()
                 if answer not in ('Y', 'N', 'y', 'n'):
                     print('invalid answer entered\ndo you want to play again? (Y/N)')
                 elif answer in ('N', 'n'):
@@ -341,7 +309,16 @@ class Game:
                 else:
                     valid = True
 
+    @classmethod
+    def run_game(cls, moves=None):
+        if moves:
+            cls.run_with_moves_list(moves)
+            return
+        cls.run_with_user_input()
+
 
 if __name__ == '__main__':
-    Game.intro()
-    Game.minesweeper(False)
+    Minesweeper.intro()
+    moves = ['2 2 f', '2 4']
+    # Minesweeper.run_game(moves)
+    Minesweeper.run_game()
--- minesweeper/test.py
+++ minesweeper/test.py
@@ -11,100 +11,128 @@ import unittest
 # mines
 
 
-class GameTest(unittest.TestCase):
+class MinesweeperTest(unittest.TestCase):
 
     def setUp(self):
-        def fixed_init(*args, **kwargs):
-            MS.Game.revealed.clear()
-            MS.Game.flagged.clear()
-            MS.Game.level = 1
-            MS.Game.size = 5
-            MS.Game.mines = 3
-            MS.Game.board = []
-            mined = [[0, 0, 0, 0, 9],
-                     [0, 9, 0, 0, 0],
-                     [0, 0, 0, 0, 0],
-                     [0, 0, 9, 0, 0],
-                     [0, 0, 0, 0, 0]]
-            MS.Game.board.extend(mined)
-
-        MS.Game.__init__ = fixed_init
-        self.game = MS.Game()
+        self.game = MS.Minesweeper(1)
+        self.game.revealed.clear()
+        self.game.flagged.clear()
+        self.game.level = 1
+        self.game.size = 5
+        self.game.mines = 4
+        self.game.board = []
+        mined = [[0, 0, 9, 0, 0],
+                 [0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 9],
+                 [0, 0, 0, 0, 0],
+                 [0, 9, 0, 0, 9]]
+        self.game.board.extend(mined)
         self.game.put_numbers()
-        moves = [('open', 2, 2), ('open', 2, 4), ('flag', 0, 4)]
-        self.modes = [x[0] for x in moves]
-        self.i = [x[1] for x in moves]
-        self.j = [x[2] for x in moves]
-        # print(self.game.play(True, ['open'], [0], [4]))
+        self.moves = ['2 2', '2 4', '0 4 f']
 
-    @classmethod
-    def tearDownClass(cls):
-        MS.Game.__init__ = MS.Game.__init__
+    def test_random_board(self):
+        for level in [1, 2, 3]:
+            with self.subTest():
+                self.game = MS.Minesweeper(level)
+                count = 0
+                for x in self.game.board:
+                    for y in x:
+                        if y == 9:
+                            count += 1
+                self.assertEqual(count, self.game.mines)
 
     @unittest.expectedFailure
-    def test_a_level_input(self):
+    def test_a_get_level(self):
         # should take 1 2 or 3 only
         for arg in [1, 2, 3]:
             with self.subTest(pattern=arg):
-                self.assertRaises(RuntimeError, MS.Game.get_level, True, arg)
+                self.assertRaises(RuntimeError, MS.Minesweeper.get_level, arg)
         # with any input that would raise an error, it goes into an infinite loop
 
-    def test_b_get_move(self):
-        # takes (True,mode,i,j), normally input
-        # returns a tuple with (mode,i,j)
-        for (mode, x, y) in (self.modes, self.i, self.j):
-            with self.subTest(pattern=(mode, x, y)):
-                self.assertEqual(self.game.get_move(True, mode, x, y), (mode, x, y))
+    def test_b_make_move(self):
+        # takes a move
+        # returns i j and key
+
+        make_move_results = [[2, 2, 'open'], [2, 4, 'open'], [0, 4, 'flag']]
+        for i in range(len(self.moves)):
+            typed = self.moves[i]
+            for j in (0, 1, 2):
+                with self.subTest(pattern=typed):
+                    tupl = self.game.make_move(typed)
+                    self.assertEqual(tupl[j], make_move_results[i][j])
         # with any input that would raise an error, it goes into an infinite loop
 
     def test_c_build(self):
         # initial build, later will be tested by mine number and empty tests
-        self.assertEqual(self.game.board, [[1, 1, 1, 1, 9],
-                                           [1, 9, 1, 1, 1],
-                                           [1, 2, 2, 1, 0],
-                                           [0, 1, 9, 1, 0],
-                                           [0, 1, 1, 1, 0]])
+        self.assertEqual(self.game.board, [[0, 1, 9, 1, 0],
+                                           [0, 1, 1, 2, 1],
+                                           [0, 0, 0, 1, 9],
+                                           [1, 1, 1, 2, 2],
+                                           [1, 9, 1, 1, 9]])
 
     def test_d1_mine(self):
         # play returns False hence ends the game then a mine is clicked
-        self.assertFalse(self.game.play(True, 'open', 0, 4))
+        self.assertFalse(self.game.play('0 2'))
+        self.game.lost()
 
     def test_d2_number(self):
         # play returns True
         # the box is added to the list 'revealed'
-        self.assertTrue(self.game.play(True, 'open', 2, 2))
-        self.assertIn((2, 2), self.game.revealed)
+        self.assertTrue(self.game.play('1 3'))
+        self.assertIn((1, 3), self.game.revealed)
 
     def test_d3_empty(self):
         # play returns True
         # the box is added to the list 'revealed'
         # surrounding empty boxes and first line of number boxes also added to revealed
-        self.assertTrue(self.game.play(True, 'open', 3, 4))
-        for (a, b) in [(1, 3), (1, 4), (2, 3), (2, 4), (3, 3), (3, 4), (4, 3), (4, 4)]:
+        self.assertTrue(self.game.play('2 2'))
+        for (a, b) in [(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3)]:
             with self.subTest(pattern=(a, b)):
                 self.assertIn((a, b), self.game.revealed)
 
     def test_e_flag(self):
         # play returns True
         # box is added to the list 'flagged'
-        self.assertTrue(self.game.play(True, 'flag', 3, 2))
+        # if same box is flagged for the second time, undo the flagging proccess
+        self.assertTrue(self.game.play('3 2 f'))
         self.assertIn((3, 2), self.game.flagged)
+        self.assertTrue(self.game.play('3 2 f'))
+        self.assertNotIn((3, 2), self.game.flagged)
 
     def test_f1_win_flag(self):
-        self.assertTrue(self.game.play(True, 'flag', 1, 1))
-        self.assertTrue(self.game.play(True, 'flag', 0, 4))
-        self.assertTrue(self.game.play(True, 'flag', 3, 2))
-        self.assertTrue(self.game.won(True))
+        self.assertTrue(self.game.play('0 0 f'))
+        self.assertFalse(self.game.won())
+        self.assertTrue(self.game.play('0 0 f'))
+        self.assertTrue(self.game.play('0 2 f'))
+        self.assertTrue(self.game.play('2 4 f'))
+        self.assertTrue(self.game.play('4 1 f'))
+        self.assertTrue(self.game.play('4 4 f'))
+        self.assertTrue(self.game.won())
 
     def test_f2_win_open(self):
-        self.assertTrue(self.game.play(True, 'open', 3, 4))
-        self.assertTrue(self.game.play(True, 'open', 4, 0))
-        self.assertTrue(self.game.play(True, 'open', 0, 0))
-        self.assertTrue(self.game.play(True, 'open', 0, 1))
-        self.assertTrue(self.game.play(True, 'open', 0, 2))
-        self.assertTrue(self.game.play(True, 'open', 0, 3))
-        self.assertTrue(self.game.play(True, 'open', 1, 0))
-        self.assertTrue(self.game.play(True, 'open', 1, 2))
-        self.assertTrue(self.game.play(True, 'open', 2, 2))
-        self.assertTrue(self.game.play(True, 'open', 4, 2))
-        self.assertTrue(self.game.won(True))
+
+        self.game.play('0 0')
+        self.game.play('0 4')
+        self.game.play('3 4')
+        self.game.play('4 0')
+        self.game.play('4 2')
+        self.game.play('4 3')
+        self.assertTrue(self.game.won())
+
+    def test_g_reveal(self):
+        self.game.play('0 0 f')
+        self.game.reveal_all()
+        for i in range(self.game.size):
+            for j in range(self.game.size):
+                self.assertIn((i, j), self.game.revealed)
+
+    def test_simple_run(self):
+        MS.Minesweeper.run_game(self.moves)
+
+    def test_h1_run_win(self):
+        moves = ['2 4 f', '4 1 f', '4 4 f', '0 2 f', '3 4']
+        MS.Minesweeper.run_game(moves)
+
+    def test_h2_run_lose(self):
+        moves = ['0 2']
+        MS.Minesweeper.run_game(moves)
